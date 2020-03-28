ee7434022eb18a3cab6519d1eead6881e17e67a1
Thomas Hooper <tomh386@gmail.com>
2020-03-28 12:35:25 -0400

    feat: added routes to registration pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,1 +1,4 @@
 export {default as LandingPage} from './landingPage'
+export {default as WholesaleProviderRegistration} from './wholesaleProviderRegistration'
+export {default as ConsumerRegistration} from './consumerRegistration'
+export {default as DeliveryAgentRegistration} from './deliveryAgentRegistration'
---
+++ src/pages/wholesaleProviderRegistration/WholesaleProviderRegistration.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const WholesaleProviderRegistration = () => {
+    return <h1>wholesale provider registration</h1>
+}
+
+export default WholesaleProviderRegistration

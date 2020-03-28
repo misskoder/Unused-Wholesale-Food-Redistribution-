8c70509112e17fc796256856049e5bc1d9720869
Thomas Hooper <tomh386@gmail.com>
2020-03-28 15:30:08 -0400

    feat: started on buyer registration form

--- src/pages/consumerRegistration/ConsumerRegistration.js
+++ src/pages/consumerRegistration/ConsumerRegistration.js
@@ -1,7 +1,32 @@
-import React from 'react'
+import React from "react";
+import { Form, Field } from "react-final-form";
+import { Input, Label } from "semantic-ui-react";
 
 const ConsumerRegistration = () => {
-    return <h1>Consumer Registration</h1>
-}
+	const onSubmit = values => {
+		console.log(values);
+	};
+	return (
+		<Form onSubmit={onSubmit}>
+			{({ handleSubmit }) => {
+				return (
+					<form onSubmit={handleSubmit}>
+						<Field
+							name="name"
+							render={() => {
+								return (
+									<>
+										<Label>Name</Label>
+										<Input />
+									</>
+								);
+							}}
+						/>
+					</form>
+				);
+			}}
+		</Form>
+	);
+};
 
-export default ConsumerRegistration
+export default ConsumerRegistration;

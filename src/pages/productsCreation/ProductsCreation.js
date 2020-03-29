ec751a474214b8ff43a1547c13bb6fb292f8a598
Thomas Hooper <tomh386@gmail.com>
2020-03-28 22:08:58 -0400

    feat: added a parent Container around the whole app

--- src/pages/productsCreation/ProductsCreation.js
+++ src/pages/productsCreation/ProductsCreation.js
@@ -1,7 +1,7 @@
 import React from "react";
-import { Container, Grid, Header, Card, Label, Input, Button } from "semantic-ui-react";
+import { Grid, Header, Card, Label, Input, Button } from "semantic-ui-react";
 import { Form, Field } from "react-final-form";
-import api from '../../api/api'
+import api from "../../api/api";
 
 const ProductsCreation = () => {
 	const { Column, Row } = Grid;
@@ -9,45 +9,42 @@ const ProductsCreation = () => {
 		console.log(values);
 	};
 	return (
-		<Container textAlign="center">
-			
-			<Form onSubmit={onSubmit}>
-				{({ handleSubmit }) => {
-					return (
-						<form onSubmit={handleSubmit}>
-							<Card fluid>
-								<Header as="h1" textAlign="center">
-									Create A Product
-								</Header>
-								<Grid divided style={{ margin: 14 }} columns={2} centered stackable>
-									<Row>
-										<Column textAlign="center">
-											<Label ribbon color="red">
-												Name
-											</Label>
-											<Field name="name" component={Input} />
-										</Column>
-										<Column textAlign="center">
-											<Label ribbon color="green">
-												Price
-											</Label>
-											<Field name="price" component={Input} />
-										</Column>
-									</Row>
-									<Row>
-										<Column textAlign="center">
-											<Button onClick={handleSubmit} color="teal">
-												Create Product
-											</Button>
-										</Column>
-									</Row>
-								</Grid>
-							</Card>
-						</form>
-					);
-				}}
-			</Form>
-		</Container>
+		<Form onSubmit={onSubmit}>
+			{({ handleSubmit }) => {
+				return (
+					<form onSubmit={handleSubmit}>
+						<Card fluid>
+							<Header as="h1" textAlign="center">
+								Create A Product
+							</Header>
+							<Grid divided style={{ margin: 14 }} columns={2} centered stackable>
+								<Row>
+									<Column textAlign="center">
+										<Label ribbon color="red">
+											Name
+										</Label>
+										<Field name="name" component={Input} />
+									</Column>
+									<Column textAlign="center">
+										<Label ribbon color="green">
+											Price
+										</Label>
+										<Field name="price" component={Input} />
+									</Column>
+								</Row>
+								<Row>
+									<Column textAlign="center">
+										<Button onClick={handleSubmit} color="teal">
+											Create Product
+										</Button>
+									</Column>
+								</Row>
+							</Grid>
+						</Card>
+					</form>
+				);
+			}}
+		</Form>
 	);
 };
 
--- src/pages/sellerRegistration/SellerRegistration.js
+++ src/pages/sellerRegistration/SellerRegistration.js
@@ -1,30 +1,28 @@
 import React from "react";
-import { Grid, Container, Icon, Card } from "semantic-ui-react";
+import { Grid, Icon, Card } from "semantic-ui-react";
 import { RegistrationForm } from "./@components";
 
 const SellerRegistration = () => {
 	const { Row, Column } = Grid;
 
 	return (
-		<Container>
-			<Card style={{ height: "350px" }} centered fluid>
-				<Grid textAlign="center" divided stackable relaxed container>
-					<Row textAlign="center">
-						<Column>
-							<h1>Seller Registration</h1>
-						</Column>
-					</Row>
-					<Row>
-						<Column verticalAlign="middle" width={8}>
-							<Icon circular color="green" size="massive" name="dollar sign" />
-						</Column>
-						<Column verticalAlign="middle" width={8}>
-							<RegistrationForm />
-						</Column>
-					</Row>
-				</Grid>
-			</Card>
-		</Container>
+		<Card style={{ height: "350px" }} centered fluid>
+			<Grid textAlign="center" divided stackable relaxed container>
+				<Row textAlign="center">
+					<Column>
+						<h1>Seller Registration</h1>
+					</Column>
+				</Row>
+				<Row>
+					<Column verticalAlign="middle" width={8}>
+						<Icon circular color="green" size="massive" name="dollar sign" />
+					</Column>
+					<Column verticalAlign="middle" width={8}>
+						<RegistrationForm />
+					</Column>
+				</Row>
+			</Grid>
+		</Card>
 	);
 };
 

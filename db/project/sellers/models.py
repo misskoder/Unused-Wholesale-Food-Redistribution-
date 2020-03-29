b03fe00dbbd62e384aea20dc038230f39687bb9f
Lesly <lesly@leslybohuchot.com>
2020-03-29 10:41:09 -0500

    class seller models updated

--- db/project/sellers/models.py
+++ db/project/sellers/models.py
@@ -6,6 +6,7 @@ class Seller(models.Model):
     name = models.CharField(max_length=200)
     address = models.CharField(max_length=200)
     phone = models.CharField(max_length=20)
+    product = models.CharField(max_length=20)
 
     def __str__(self):
         return self.name
